d4c52d1d4da7f4274e8e9936bb1d358af5854f43
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-04-18 12:46:54 +0200

    hold dice

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,23 +1,50 @@
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
 
-  function rollDice() {
-    setDice(allNewDice());
+  function generateNewDice() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
   }
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({ value: Math.ceil(Math.random() * 6), isHeld: false });
+      newDice.push(generateNewDice());
     }
     return newDice;
   }
 
-  const dices = dice.map((die) => <Die value={die.value} />);
+  function rollDice() {
+    setDice((oldDice) =>
+      oldDice.map((dice) => {
+        return dice.isHeld ? dice : generateNewDice();
+      })
+    );
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((dice) => {
+        return dice.id === id ? { ...dice, isHeld: !dice.isHeld } : dice;
+      })
+    );
+  }
+  const dices = dice.map((die) => (
+    <Die
+      value={die.value}
+      key={die.id}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
 
   return (
     <main>
--- tenzies/src/components/Die.jsx
+++ tenzies/src/components/Die.jsx
@@ -1,8 +1,12 @@
 import "../components/Die.css";
 
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+
   return (
-    <div className="die__item">
+    <div className="die__item" style={styles} onClick={props.holdDice}>
       <h2>{props.value}</h2>
     </div>
   );
