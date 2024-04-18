b975a5e66a3c4055a5119c070fba502dea0184c4
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-04-18 11:21:51 +0200

    Updating the array of numbers in state to be an array of objects instead

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -5,20 +5,26 @@ import { useState } from "react";
 function App() {
   const [dice, setDice] = useState(allNewDice());
 
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({ value: Math.ceil(Math.random() * 6), isHeld: false });
     }
     return newDice;
   }
 
-  const dices = dice.map((die) => <Die value={die} />);
+  const dices = dice.map((die) => <Die value={die.value} />);
 
   return (
     <main>
       <div className="dice__container">{dices}</div>
-      <button onClick={() => setDice(allNewDice())}>Roll Dice</button>
+      <button onClick={rollDice} className="dice__btn">
+        Roll
+      </button>
     </main>
   );
 }
